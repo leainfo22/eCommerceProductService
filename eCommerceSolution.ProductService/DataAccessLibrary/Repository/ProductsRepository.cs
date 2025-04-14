cf42047693b71af4dfb4618d225520ad35a3e412
leainfo22 <leandro.vasquez.mora@gmail.com>
2025-04-14 11:23:05 +1000

    Adding Product interface and its functionality in the DB

---
+++ eCommerceSolution.ProductService/DataAccessLibrary/Repository/ProductsRepository.cs
@@ -0,0 +1,56 @@
+﻿using DataAccessLayer.Entities;
+using DataAccessLayer.RepositoryContracts;
+using eCommerce.DataAccessLayer.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace DataAccessLayer.Repository;
+
+public class ProductsRepository : IProductsRepository
+{
+
+    private readonly ApplicationDbContext _dbContext;
+
+
+    // Constructor to initialize the DbContext 
+    public ProductsRepository(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+
+    public async Task<Product?> AddlProduct(Product product)
+    {
+        _dbContext.Products.Add(product);
+        await _dbContext.SaveChangesAsync();
+        return product;
+    }
+
+    public async Task<bool> DeleteProduct(Guid productID)
+    {
+        Product? product = await _dbContext.Products.FirstOrDefaultAsync(p => 
+            p.ProductID == productID);
+        if (product != null)
+            return false;
+        
+        _dbContext.Products.Remove(product);
+        await _dbContext.SaveChangesAsync();
+        return true;
+    }
+
+    public Task<IEnumerable<Product>> GetAllProduct()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Product?>> GetProductByCondition(Expression<Func<Product, bool>> conditionExpression)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Product?> UpdateProduct(Product product)
+    {
+        throw new NotImplementedException();
+    }
+}
+
---
+++ eCommerceSolution.ProductService/DataAccessLibrary/RepositoryContracts/IProductsRepository.cs
@@ -0,0 +1,45 @@
+﻿using DataAccessLayer.Entities;
+using System.Linq.Expressions;
+
+namespace DataAccessLayer.RepositoryContracts;
+
+public interface IProductsRepository
+{
+    /// <summary>
+    /// Get all products from the database
+    /// </summary>
+    /// <returns>All products</returns>
+    Task<IEnumerable<Product>> GetAllProduct();
+
+    /// <summary>
+    /// Retrieves all products based on the specified condition asynchronously.
+    /// </summary>
+    /// <param name="conditionExpression"></param>
+    /// <returns> Return a collection of matching products  </returns>
+    Task<IEnumerable<Product?>>
+    GetProductByCondition
+    (Expression<Func<Product, bool>> conditionExpression);
+
+    /// <summary>
+    /// Add a new product to the database
+    /// </summary>
+    /// <returns>Return the product if the function was success, if not it will return null</returns>
+    Task<Product?> AddlProduct(Product product);
+
+    /// <summary>
+    /// Update a new product to the database
+    /// </summary>
+    /// <returns>Return the product if the function was success, if not it will return null</returns>
+    Task<Product?> UpdateProduct(Product product);
+
+    /// <summary>
+    /// Delete a new product to the database
+    /// <param name="productID"></param>
+    /// </summary>
+    /// <returns>Return the product if the function was success, if not it will return null</returns>
+    Task<bool> DeleteProduct(Guid productID);
+
+
+
+}
+
